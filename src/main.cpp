60bdc05c48c0bb4ad6dc3f2305e0dfb0d8190ffa
James Taylor <soonermagic32@gmail.com>
2025-04-05 21:57:13 -0500

    step one completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
 
 int main(int argc, char*argv[]) {
     bool printBytes = false;
     std::string fileName;
+	int c = 0;
     if(argc > 1) {
-        for(int i = 1; i < argc; i++) {
-            std::string arg = argv[i];
-            if(arg.rfind("-", 0) == 0) {
-                std::cout << "argument" << std::endl;
-            }
-            else {
-                std::cout << "file" << std::endl;
-            }
-        }
+		while((c = getopt(argc, argv, "c:")) != -1) {
+			switch(c) {
+				case 'c':
+					printBytes = true;
+					if(*optarg == 'c') {
+						std::cout << "-c requires an argument" << std::endl;
+						return -1;
+					}
+					fileName = optarg;
+					break;
+				default:
+					std::cout << "Invalid arguments" << std::endl;
+			}
+		}
     }
+
+	if(printBytes) {
+		std::ifstream in(fileName, std::ios::binary | std::ios::ate);
+		if(!in.is_open()) {
+			std::cout << "Unable to open " << fileName << std::endl;
+			return -1;
+		}
+		auto size = in.tellg();
+		std::cout << "  " << size << " " << fileName << std::endl;
+	}
     
     return 0;
 }
