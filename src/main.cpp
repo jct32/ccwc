0643dbf6a33367516f99e6cde24c560cd0080d95
James Taylor <soonermagic32@gmail.com>
2025-04-06 09:14:25 -0500

    Added -l argument

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,29 @@
 
 int main(int argc, char*argv[]) {
     bool printBytes = false;
+    bool printLines = false;
     std::string fileName;
 	int c = 0;
+    //TODO: Allow for multiple arguments and still work
     if(argc > 1) {
-		while((c = getopt(argc, argv, "c:")) != -1) {
+		while((c = getopt(argc, argv, "l:c:")) != -1) {
 			switch(c) {
 				case 'c':
-					printBytes = true;
 					if(*optarg == 'c') {
 						std::cout << "-c requires an argument" << std::endl;
 						return -1;
 					}
 					fileName = optarg;
+					printBytes = true;
 					break;
+                case 'l':
+                    if(*optarg == 'l') {
+                        std::cout << "-l requires an argument" << std::endl;
+                        return -1;
+                    }
+                    fileName = optarg;
+                    printLines = true;
+                    break;
 				default:
 					std::cout << "Invalid arguments" << std::endl;
 			}
@@ -30,8 +40,21 @@ int main(int argc, char*argv[]) {
 			return -1;
 		}
 		auto size = in.tellg();
-		std::cout << "  " << size << " " << fileName << std::endl;
+		std::cout << size << " " << fileName << std::endl;
 	}
+    if(printLines) {
+        std::ifstream in(fileName);
+        if(!in.is_open()) {
+            std::cout << "Unable to open " << fileName << std::endl;
+            return -1;
+        }
+        int lines = 0;
+        std::string line;
+        while(std::getline(in, line)) {
+            lines++;
+        }
+        std::cout  << lines << " " << fileName << std::endl;
+    }
     
     return 0;
 }
